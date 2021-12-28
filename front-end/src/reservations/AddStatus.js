a42621a83ab934c0fc9feaa68354b02769f62e35
Todd White <todddeakron@gmail.com>
2021-12-28 17:09:28 -0500

    Fixed re-render bug

--- front-end/src/reservations/AddStatus.js
+++ front-end/src/reservations/AddStatus.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+
 import ErrorCaught from "./ErrorCaught";
 
 /**
@@ -9,19 +9,15 @@ import ErrorCaught from "./ErrorCaught";
  * @returns {JSX.Element}
  */
 function AddStatus({ date }) {
-  const [statuses, setStatuses] = useState([]);
+  const [statuses, setStatuses] = useState([
+    { statusId: 1, statusName: "Had RC" },
+    { statusId: 2, statusName: "Start MAT" },
+    { statusId: 3, statusName: "On Restrictions" },
+  ]);
 
   //State vars for ErrorCaught
   const [visibility3, setVisibility3] = useState(null);
   const [errMessage, setErrMessage] = useState("");
-
-  //Populate the statuses array, this will be replaced when I implement the api call
-  let statusArray = [
-    { statusId: 1, statusName: "Had RC" },
-    { statusId: 2, statusName: "Start MAT" },
-    { statusId: 3, statusName: "On Restrictions" },
-  ];
-  setStatuses(statusArray);
 
   //Create table rows from the statuses state array and use to populate the drop-down
   const statusLinks = statuses.map((status) => (
@@ -33,11 +29,13 @@ function AddStatus({ date }) {
     <main>
       <h1>Add Status Event</h1>
       <div class="form-group">
-        <label for="exampleFormControlSelect1">Select Status</label>
-        <select class="form-control" id="statusId" name="statusId">
-          <option value="">--Select an Option--</option>
-          {statusLinks}
-        </select>
+        <form>
+          <label for="exampleFormControlSelect1">Select Status</label>
+          <select class="form-control" id="statusId" name="statusId">
+            <option value="">--Select an Option--</option>
+            {statusLinks}
+          </select>
+        </form>
       </div>
       <ErrorCaught visibility3={visibility3} msg={errMessage} />
     </main>
