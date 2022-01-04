60644177737f3324509d0b039cc7814741a995a9
Todd White <todddeakron@gmail.com>
2022-01-04 16:49:14 -0500

    Started RecCheck component

---
+++ front-end/src/reservations/RecCheck.js
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from "react";
+import { createStatus } from "../utils/api";
+import ErrorCaught from "./ErrorCaught";
+import { readParticipant } from "../utils/api";
+import { useParams, useHistory } from "react-router-dom";
+
+/**
+ * Defines the dashboard page.
+ * @param date
+ *  the date for which the user wants to view reservations.
+ * @returns {JSX.Element}
+ */
+function AddStatus({ date }) {
+  const [statuses, setStatuses] = useState([
+    { statusId: 1, statusName: "Had RC" },
+    { statusId: 2, statusName: "Start MAT" },
+    { statusId: 3, statusName: "On Restrictions" },
+  ]);
+
+  //Create state variables for each field of reservation and add event listeners
+  const [firstName, setFirstName] = useState("");
+  const handleFirstNameChange = (event) => setFirstName(event.target.value);
+
+  const [lastName, setLastName] = useState("");
+  const handleLastNameChange = (event) => setLastName(event.target.value);
+
+  const [incident, setIncident] = useState(null);
+  const handleIncidentChange = (event) => setIncident(event.target.value);
+
+  const [statusName, setStatusName] = useState(null);
+  const handleStatusNameChange = (event) => setStatusName(event.target.value);
+
+  const [startDate, setStartDate] = useState(null);
+  const handleStartDateChange = (event) => setStartDate(event.target.value);
+
+  const [dischargeDate, setDischargeDate] = useState(null);
+  const handleDischargeDateChange = (event) =>
+    setDischargeDate(event.target.value);
+
+  const [checkDate, setCheckDate] = useState(null);
+  const handleCheckDateChange = (event) => setCheckDate(event.target.value);
+
+  const [timePeriod, setTimePeriod] = useState(null);
+  const handleTimePeriodChange = (event) => setTimePeriod(event.target.value);
+
+  //State vars for ErrorCaught
+  const [visibility3, setVisibility3] = useState(null);
+  const [errMessage, setErrMessage] = useState("");
+
+  //Get ParticipantId from url
+  const { participantId, incidentId } = useParams();
+
+  //Make an API Call to get the reservation based on the reservation_id
+  useEffect(() => {
+    async function getParticipant(participantId) {
+      const response = await readParticipant(participantId);
+
+      setFirstName(response.first_name);
+      setLastName(response.last_name);
+      setIncident(incidentId);
+    }
+    getParticipant(participantId);
+  }, [participantId]);
+
+  async function handleSubmit() {
+    //Make an api call to post the new table to the db
+    let status = {
+      data: {},
+    };
+
+    status.data.participant_id = participantId;
+    status.data.instance_id = incidentId;
+    status.data.status_name = statusName;
+    status.data.date = statusDate;
+
+    try {
+      const response = await createStatus(status);
+    } catch (err) {
+      console.log("Error making createTable API call: ", err);
+    }
+  }
+
+  //Create table rows from the statuses state array and use to populate the drop-down
+  const statusLinks = statuses.map((status) => (
+    <option value={status.statusName}>{status.statusName}</option>
+  ));
+
+  //Return the html with status drop-down
+  return (
+    <main>
+      <h1>Recidivism Check</h1>
+      <div className="form-group">
+        <label htmlFor="first_name">First Name</label>
+        <input
+          type="text"
+          name="first_name"
+          className="form-control"
+          id="first_name"
+          aria-describedby="emailHelp"
+          onChange={handleFirstNameChange}
+          value={firstName}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="last_name">Last Name</label>
+        <input
+          type="text"
+          name="last_name"
+          className="form-control"
+          id="last_name"
+          onChange={handleLastNameChange}
+          value={lastName}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="last_name">Incident</label>
+        <input
+          type="text"
+          name="last_name"
+          className="form-control"
+          id="last_name"
+          onChange={handleIncidentChange}
+          value={incident}
+        />
+      </div>
+      <div class="form-group">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="start_date">Start Date</label>
+            <input
+              type="date"
+              name="start_date"
+              className="form-control"
+              id="start_date"
+              onChange={handleStartDateChange}
+              value={startDate}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="discharge_date">Discharge Date</label>
+            <input
+              type="date"
+              name="discharge_date"
+              className="form-control"
+              id="discharge_date"
+              onChange={handleDischargeDateChange}
+              value={dischargeDate}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="check_date">Date of Recidivism Check</label>
+            <input
+              type="date"
+              name="check_date"
+              className="form-control"
+              id="check_date"
+              onChange={handleCheckDateChange}
+              value={checkDate}
+            />
+          </div>
+          <label for="exampleFormControlSelect1">
+            Select Time Period of Check
+          </label>
+          <select
+            class="form-control"
+            id="timePeriod"
+            name="timePeriod"
+            onChange={handleTimePeriodChange}
+            value={timePeriod}
+          >
+            <option value="">--Time Period--</option>
+            <option value="1"> 1 Year</option>
+            <option value="2"> 2 Year</option>
+            <option value="3"> 3 Year</option>
+            <option value="4"> 4 Year</option>
+            <option value="5"> 5 Year</option>
+          </select>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+      <ErrorCaught visibility3={visibility3} msg={errMessage} />
+    </main>
+  );
+}
+
+export default AddStatus;
